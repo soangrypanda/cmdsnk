23b0c0a277031bcc5aad3d3405a31eed1c4422de
Anton Tretiakov <risen sap at gmail dot com>
2020-10-24 16:55:18 +0300

    twocoords method peototypes put in twotypes.r

--- twocoord.r
+++ twocoord.r
@@ -3,6 +3,10 @@
 
 #include <object.r>
 
+void *twocoord_ctor(void *const _self, va_list *app);
+void twocoord_print(const void *const _self);
+int twocoord_compare(const void *const _self, const void *const _other);
+
 #define TWOCOORD_VT          OBJECT_VT
 
 #define TWOCOORD_VT_INIT     OBJECT_VT_INIT,                              \
